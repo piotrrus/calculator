a6c2309c2ce5526ad60f9920f1986e626a7ee26e
piotrrus <irache@wp.pl>
2024-07-13 22:48:18 +0200

    Add files via upload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { createReducer, provideState, provideStore } from '@ngrx/store';
-import { calculationReducer } from '@org/store';
+import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
-
-
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideStore(),
+    provideHttpClient(),
     provideStoreDevtools({
       maxAge: 25, // Retains last 25 states
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-      connectInZone: true // If set to true, the connection is established within the Angular zone
-    })
+      connectInZone: true, // If set to true, the connection is established within the Angular zone
+    }),
   ],
 };
